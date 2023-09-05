5fba407357f3004d59cc163c5510f1cd5a060eb0
Jonathan Grabowski <jon.grabowski@gmail.com>
2023-09-05 17:23:49 -0400

    updated db for September

--- src/components/PageContent.js
+++ src/components/PageContent.js
@@ -1,39 +1,58 @@
 import { useState, useEffect } from 'react'
 
 function PageContent() {
-    const black_state = {1: {haiku: "",}}
-    const [haikus, setHaikus] = useState(black_state)
+    const [haikusArray, setHaikus] = useState([])
+    const [todayHaiku, setTodayHaiku] = useState({})
 
     useEffect(() => {
-        fetch(`http://localhost:3001/July_2023`)
+        fetch(`http://localhost:3001/${month_text}`)
         .then(res => res.json())
-        .then(julyHaikus => setHaikus(julyHaikus))
+        .then(Haikus => {
+            // console.log(Haikus)
+            setHaikus(Haikus)
+
+            }
+        )
     }, [])
+
+    useEffect(() => {
+        findHaiku(haikusArray)
+    }, [haikusArray])
     
     const date = new Date()
-    const day = date.getDate()
+    const currentDay = date.getDate()
     const month = date.getMonth() + 1
     const month_text = getMonthName(month)
     const year = date.getFullYear()
-    const currentDate = `${month_text} ${day}, ${year}`
-    // console.log(haikus[day].haiku)
+    const currentDate = `${month_text} ${currentDay}, ${year}`
     // console.log(day)
     function getMonthName(monthNumber) {
         const date = new Date();
         date.setMonth(monthNumber - 1);
         return date.toLocaleString('en-US', { month: 'long' });
       }
-      
+    
+    function findHaiku(haikus) {
+        haikus.forEach(haiku => {
+            if (haiku.date === `${month_text} ${currentDay}`) {
+                console.log(haiku)
+                setTodayHaiku(haiku)
+            }
+        }
+        )
+    }
     return (
         <div>
             <div>
                 <h2>{currentDate}</h2>
+                <h3>{todayHaiku.day}</h3>
             </div>
             <div id="haiku_image">
                 <img src="https://png.pngtree.com/png-clipart/20221028/original/pngtree-under-construction-png-image_8741247.png" alt="placeholder"></img>
             </div>
             <div id="haiku_container">
-                <h3>{haikus[1].haiku == "" ? "" : haikus[day].haiku}</h3>
+                <h2>{todayHaiku.body}</h2>
+                {/* <h3>{haikus.haiku}</h3> */}
             </div>    
         </div>
     )
