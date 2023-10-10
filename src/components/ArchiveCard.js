6f2980fd22b9f0c6380e610af3b64b2331c6fdae
Jonathan Grabowski <jon.grabowski@gmail.com>
2023-10-10 17:00:40 -0400

    archive card click for front/back

--- src/components/ArchiveCard.js
+++ src/components/ArchiveCard.js
@@ -1,22 +1,40 @@
+import { useState } from "react"
+
 function ArchiveCard({haiku, selectedMonth}) {
     const {date, day, body} = haiku
+    const[cardTrigger, setCardTrigger] = useState(true)
+
+    function handleClick() {
+        setCardTrigger(!cardTrigger)
+    }
 
     const bodyArray = body.split('|')
         
         const bodyLines = bodyArray.map(line => {
             line = line.trim()
-            return <p key={line} className='my-1 fs-5 card-text'>{line}</p>
+            return <p key={line} className='my-1 fs-5 card-text'><strong>{line}</strong></p>
         })
 
     return(
-        <div className="card archive-card m-3 rounded shadow border border-3 border-black">
-            <div className="card-body rounded">
-                <h5 className="card-title text-wrap">{day}</h5>
-                <h6 className="card-subtitle border-bottom border-black pb-2 mb-4 text-body-secondary">{selectedMonth + ' ' + date}</h6>
-                <div className='pb-3'>
-                    {bodyLines}
+        <div>
+            {cardTrigger ? 
+            <div 
+            className="card archive-card m-3 rounded shadow border border-3 border-black" 
+            onClick={handleClick}>
+                <div className="card-body rounded">
+                    <h5 className="card-title pb-1 pt-1 mb-5 fs-3 border-bottom border-1 border-black">{selectedMonth + ' ' + date}</h5>
+                    <h6 className="card-subtitle text-wrap text-body-secondary fs-4">{day}</h6>
                 </div>
             </div>
+            : 
+            <div className="card archive-card m-3 rounded shadow border border-3 border-black" onClick={handleClick}>
+                <div className="card-body rounded">
+                    <h5 className="card-title pb-1 pt-1 mb-5 fs-3 border-bottom border-1 border-black">{day}</h5>
+                    <div className='pb-3'>
+                        {bodyLines}
+                    </div>
+                </div>
+            </div>}
         </div>
     )
 }
